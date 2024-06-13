bf940d502831f76c4fe7530841f571164cfcfe5f
djuvanemartin@gmail.com <djuvanemartin@gmail.com>
2024-06-12 20:02:48 -0500

    upload final version of app.py_2

--- app.py
+++ app.py
@@ -63,42 +63,60 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     session = get_session()
-    results = session.query(Station.station).all()
+    results = session.query(Station.station, Station.name).all()
     session.close()
 
-    stations = [result[0] for result in results]
-    return jsonify(stations)
+    station_list = []
+    for station, name in results:
+        station_dict = {}
+        station_dict["station"] = station
+        station_dict["name"] = name
+        station_list.append(station_dict)
+
+    return jsonify(station_list)
+
 
 # TOBS route
 @app.route("/api/v1.0/tobs")
 def tobs():
     session = get_session()
-    most_active_station = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()[0]
+    most_active_station = session.query(Measurement.station, func.count(Measurement.station))\
+                                 .group_by(Measurement.station)\
+                                 .order_by(func.count(Measurement.station).desc())\
+                                 .first()[0]
     most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
     most_recent_date = dt.datetime.strptime(most_recent_date, "%Y-%m-%d")
     one_year_ago = most_recent_date - dt.timedelta(days=365)
 
-    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == most_active_station).filter(Measurement.date >= one_year_ago).all()
+    results = session.query(Measurement.date, Measurement.tobs)\
+                     .filter(Measurement.station == most_active_station)\
+                     .filter(Measurement.date >= one_year_ago).all()
     session.close()
 
     tobs_data = {date: tobs for date, tobs in results}
     return jsonify(tobs_data)
 
+
 # Start date route
-@app.route("/api/v1.0/<start>")
-def start(start):
+@app.route("/api/v1.0/tobs")
+def tobs():
     session = get_session()
-    start_date = dt.datetime.strptime(start, "%Y-%m-%d")
-    
-    results = session.query(
-        func.min(Measurement.tobs),
-        func.avg(Measurement.tobs),
-        func.max(Measurement.tobs)
-    ).filter(Measurement.date >= start_date).all()
+    most_active_station = session.query(Measurement.station, func.count(Measurement.station))\
+                                 .group_by(Measurement.station)\
+                                 .order_by(func.count(Measurement.station).desc())\
+                                 .first()[0]
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    most_recent_date = dt.datetime.strptime(most_recent_date, "%Y-%m-%d")
+    one_year_ago = most_recent_date - dt.timedelta(days=365)
+
+    results = session.query(Measurement.date, Measurement.tobs)\
+                     .filter(Measurement.station == most_active_station)\
+                     .filter(Measurement.date >= one_year_ago).all()
     session.close()
-    
-    temps = list(np.ravel(results))
-    return jsonify(temps)
+
+    tobs_data = {date: tobs for date, tobs in results}
+    return jsonify(tobs_data)
+
 
 # Start-end date route
 @app.route("/api/v1.0/<start>/<end>")
@@ -116,6 +134,7 @@ def start_end(start, end):
     
     temps = list(np.ravel(results))
     return jsonify(temps)
+
 
 #################################################
 # Flask Routes
